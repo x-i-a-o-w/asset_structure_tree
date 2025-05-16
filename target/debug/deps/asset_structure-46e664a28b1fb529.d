8905489bf179184d197a38633b9ab0ee4ad96e60
x-i-a-o-w <xiaoming.rustdev@gmail.com>
2025-05-16 23:40:56 +0200

    removed target

--- target/debug/deps/asset_structure-46e664a28b1fb529.d
+++
@@ -1,9 +0,0 @@
-/home/ygg_/asset_structure/target/debug/deps/libasset_structure-46e664a28b1fb529.rmeta: src/lib.rs Cargo.toml
-
-/home/ygg_/asset_structure/target/debug/deps/asset_structure-46e664a28b1fb529.d: src/lib.rs Cargo.toml
-
-src/lib.rs:
-Cargo.toml:
-
-# env-dep:CLIPPY_ARGS=
-# env-dep:CLIPPY_CONF_DIR
